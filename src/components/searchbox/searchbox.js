e90329b1cf992a66e5cf8e7893bbf1098cbcaef9
aayushbisen <aayush23101998@outlook.com>
2020-03-19 00:32:16 +0530

    Final App

---
+++ src/components/searchbox/searchbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './searchbox.css';
+
+const SearchBox = ({placeholder, handleChange  }) => {
+    return (
+        <input className='search' type='search' onChange={handleChange} placeholder={placeholder} />
+
+    );
+}
+
+export default SearchBox;
