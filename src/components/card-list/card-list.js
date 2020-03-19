2185377e21d6efbeb70c52b1d0985966a8579f55
aayushbisen <aayush23101998@outlook.com>
2020-03-19 11:58:49 +0530

    Deployed successfully to gh-pages

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -4,14 +4,8 @@ import Card from '../card/card';
 
 import './card-list.css';
 
-const CardList = (props) => {
-    return (
-        <div className='card-list'>
-            {
-                props.monsters.map(monster => <Card key={monster.id} monster={monster} />)
-            }
-        </div>
-    );
+const CardList = ({ monsters }) => {
+	return <div className="card-list">{monsters.map((monster) => <Card key={monster.id} monster={monster} />)}</div>;
 };
 
 export default CardList;
--- src/components/searchbox/searchbox.js
+++ src/components/searchbox/searchbox.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 import './searchbox.css';
 
-const SearchBox = ({placeholder, handleChange  }) => {
-    return (
-        <input className='search' type='search' onChange={handleChange} placeholder={placeholder} />
-
-    );
-}
+const SearchBox = ({ placeholder, handleChange }) => {
+	return <input className="search" type="search" onChange={handleChange} placeholder={placeholder} />;
+};
 
 export default SearchBox;
