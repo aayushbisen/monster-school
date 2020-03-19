319ae19b46771c4016fffb0b2165cfa12b87dc96
aayushbisen <aayush23101998@outlook.com>
2020-03-19 11:57:00 +0530

    Updates

---
+++ precache-manifest.8374d5a6b74fa9f86529e3cbc7c58a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd26b9ccaec70adc5f63238c10d55da",
+    "url": "/monster-school/index.html"
+  },
+  {
+    "revision": "dd6033c5bb962e3f6838",
+    "url": "/monster-school/static/css/main.2eceed14.chunk.css"
+  },
+  {
+    "revision": "65463a61f379eea9d173",
+    "url": "/monster-school/static/js/2.0b3b7acc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-school/static/js/2.0b3b7acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6033c5bb962e3f6838",
+    "url": "/monster-school/static/js/main.0188a175.chunk.js"
+  },
+  {
+    "revision": "418df4111fa609c98881",
+    "url": "/monster-school/static/js/runtime-main.c0c99ebb.js"
+  }
+]);
